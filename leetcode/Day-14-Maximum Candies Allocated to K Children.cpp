2fd2e02854b93fbbebb153fc03535d7e7f1a3105
Udit Sharma <109049436+UditSharma04@users.noreply.github.com>
2025-03-15 19:33:30 +0530

    😊 update

---
+++ leetcode/Day-14-Maximum Candies Allocated to K Children.cpp
@@ -0,0 +1,25 @@
+class Solution {
+    public:
+        int maximumCandies(vector<int>& candies, long long k) {
+            long long left = 1, right = *max_element(candies.begin(), candies.end());
+            int result = 0;
+    
+            while (left <= right) {
+                long long mid = left + (right - left) / 2;
+                long long cnt = 0;
+    
+                for (int pile : candies) {
+                    cnt += pile / mid;
+                }
+    
+                if (cnt >= k) {
+                    result = mid;
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+    
+            return result;
+        }
+    };
